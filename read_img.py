0ce022fcc63a080c8b595b3971e2529db9118358
Matt <matthew.j.beadle@gmail.com>
2023-02-19 15:45:29 -0600

    convert to function

--- read_img.py
+++ read_img.py
@@ -4,33 +4,39 @@ import numpy as np
 from PIL import Image 
 import pandas as pd 
 
+def convert_img_to_df_histogram(file_name:str) -> pd.DataFrame:
+
+    raw_data = rawpy.imread(file_name)
+    # Convert raw into readable array.
+    img = Image.fromarray(raw_data.postprocess())
+
+    # Resize image
+    img = img.resize([500,500])
+
+    ## Convert to rgb hisogram.
+    r, g, b = img.split()
+    r = r.histogram()
+    g = g.histogram()
+    b = b.histogram()
+
+    ## Convert to datafame 
+
+    image_series = pd.concat([
+        pd.Series(r, index=['red_' + str(i) for i in range(256)]),
+        pd.Series(g, index=['green_' + str(i) for i in range(256)]),
+        pd.Series(b, index=['blue_' + str(i) for i in range(256)])
+    ])
+
+    df = pd.DataFrame(image_series).transpose()
+    df.index = [file.split('.')[0]]
+
+    return df 
+
+
 # %%
+if __name__ == '__main__':
+    file = 'DSC_0002.NEF'
 
-file = 'DSC_0002.NEF'
+    df_hist = convert_img_to_df_histogram(file)
 
-raw_data = rawpy.imread(file)
-
-img = Image.fromarray(raw_data.postprocess())
-# %%
-
-img = img.resize([100,100])
-# %%
-
-## Convert to rgb hisogram.
-
-r, g, b = img.split()
-
-r = r.histogram()
-g = g.histogram()
-b = b.histogram()
-# %%
-
-image_series = pd.concat([
-    pd.Series(r, index=['red_' + str(i) for i in range(256)]),
-    pd.Series(g, index=['green_' + str(i) for i in range(256)]),
-    pd.Series(b, index=['blue_' + str(i) for i in range(256)])
-])
-
-df = pd.DataFrame(image_series).transpose()
-df.index = [file.split('.')[0]]
 # %%
